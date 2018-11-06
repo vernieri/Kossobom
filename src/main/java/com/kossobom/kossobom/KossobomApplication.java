675bd210398b7d7ab8b5b03c9164a8fe9dd7f8bf
Vernieri <jvmendes.ti@gmail.com>
2018-11-06 16:28:19 -0200

    Create KossobomApplication.java
    First commit

---
+++ src/main/java/com/kossobom/kossobom/KossobomApplication.java
@@ -0,0 +1,12 @@
+package com.kossobom.kossobom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KossobomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KossobomApplication.class, args);
+	}
+}
