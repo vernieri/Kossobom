ba597aa28857e4320100dd8f6dc09d0646bd90d2
Vernieri <jvmendes.ti@gmail.com>
2018-11-08 17:55:43 -0200

    Create Evento.java

---
+++ src/main/java/com/kossobom/kossobom/models/Evento.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kossobom.kossobom.models;
+
+/**
+ *
+ * @author Joao TI
+ */
+public class Evento {
+    
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }    
+    
+}
