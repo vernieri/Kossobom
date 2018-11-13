b34933b35be56f13eaf2bdf12ccb7572fdcf928e
Vernieri <jvmendes.ti@gmail.com>
2018-11-13 14:57:05 -0200

    Update Evento.java

--- src/main/java/com/kossobom/kossobom/models/Evento.java
+++ src/main/java/com/kossobom/kossobom/models/Evento.java
@@ -5,16 +5,38 @@
  */
 package com.kossobom.kossobom.models;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Joao TI
  */
-public class Evento {
+@Entity
+public class Evento implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
+  
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
     
     private String nome;
     private String local;
     private String data;
     private String horario;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }    
     
     public String getNome() {
         return nome;
