70dc1c5b6ff351f7fefc1ba4b13791df0708134e
Vernieri <jvmendes.ti@gmail.com>
2018-12-06 14:38:29 -0200

    Update EventoController.java

--- src/main/java/com/kossobom/kossobom/controllers/EventoController.java
+++ src/main/java/com/kossobom/kossobom/controllers/EventoController.java
@@ -12,10 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- *
- * @author Joao TI
- */
 @Controller
 public class EventoController {
     
